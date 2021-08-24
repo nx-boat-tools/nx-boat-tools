5deb34a2db4476414ca237d191d95436280674ef
Killian Hale <hale.killian@gmail.com>
2021-08-23 23:52:34 -0400

    gitignore fixes and git clean

---
+++ src/e2e/common-e2e/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'common-e2e',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/e2e/common-e2e',
+};
