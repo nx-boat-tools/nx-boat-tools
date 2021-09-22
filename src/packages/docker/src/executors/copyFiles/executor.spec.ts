5dacfbe2450f7f376e7a06f9c4d94f418726fd9c
Killian Hale <hale.killian@gmail.com>
2021-09-22 02:30:58 -0400

    fixing docker tests compilation

--- src/packages/docker/src/executors/copyFiles/executor.spec.ts
+++ src/packages/docker/src/executors/copyFiles/executor.spec.ts
@@ -1,11 +1,25 @@
 import { BuildExecutorSchema } from './schema';
 import executor from './executor';
+import { ExecutorContext } from '@nrwl/devkit';
 
-const options: BuildExecutorSchema = {};
+const options: BuildExecutorSchema = {
+  distPath: './dist/my-project'
+};
+
+const context: ExecutorContext = {
+  root: __dirname,
+  projectName: "my-project",
+  targetName: undefined,
+  configurationName: undefined,
+  target: undefined,
+  workspace: undefined,
+  cwd: __dirname,
+  isVerbose: false,
+}
 
 describe('Build Executor', () => {
   it('can run', async () => {
-    const output = await executor(options);
+    const output = await executor(options, context);
     expect(output.success).toBe(true);
   });
 });
--- src/packages/docker/src/executors/publish/executor.spec.ts
+++ src/packages/docker/src/executors/publish/executor.spec.ts
@@ -1,11 +1,26 @@
-import { BuildExecutorSchema } from './schema';
+import { PublishExecutorSchema } from './schema';
 import executor from './executor';
+import { ExecutorContext } from '@nrwl/devkit';
 
-const options: BuildExecutorSchema = {};
+const options: PublishExecutorSchema = {
+  buildPath: './apps/my-project',
+  dockerRepoOrUser: 'myusername'
+};
+
+const context: ExecutorContext = {
+  root: __dirname,
+  projectName: "my-project",
+  targetName: undefined,
+  configurationName: undefined,
+  target: undefined,
+  workspace: undefined,
+  cwd: __dirname,
+  isVerbose: false,
+}
 
 describe('Build Executor', () => {
   it('can run', async () => {
-    const output = await executor(options);
+    const output = await executor(options, context);
     expect(output.success).toBe(true);
   });
 });
