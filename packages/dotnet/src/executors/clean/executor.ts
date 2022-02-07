f07551f862e8930c25d2acd2e7db6b24171b178d
Killian Hale <hale.killian@gmail.com>
2022-02-06 20:54:48 -0500

    fix(dotnet): fixing executors and srcPath (#50)
    * fix(dotnet): fixing executors and srcPath
    
    * fix(dotnet): fixing tests due to srcPath change
    
    * fix(misc): yarn.lock rebase fix

--- packages/dotnet/src/executors/clean/executor.ts
+++ packages/dotnet/src/executors/clean/executor.ts
@@ -1,9 +1,8 @@
-import * as _ from 'underscore';
-import { ExecutorContext, runExecutor } from '@nrwl/devkit';
-import { asyncIteratorToArray } from '@nx-boat-tools/common';
+import { ExecutorContext } from '@nrwl/devkit';
 
 import { CleanDotnetExecutorSchema } from './schema';
 import { DotNetCommandExecutorSchema } from '../run-dotnet-command/schema';
+import { runDotnetCommand } from '../run-dotnet-command/executor';
 
 export default async function run(
   options: CleanDotnetExecutorSchema,
@@ -14,21 +13,5 @@ export default async function run(
     action: 'clean',
   };
 
-  const asyncResults = await runExecutor(
-    {
-      project: context.projectName,
-      target: 'run-dotnet-command',
-      configuration: context.configurationName,
-    },
-    dotnetOptions,
-    context
-  );
-
-  const results = await asyncIteratorToArray(asyncResults);
-
-  const success = _.all(results, (r) => r.success === true);
-
-  if (!success) throw new Error('Executor failed');
-
-  return { success: true };
+  return await runDotnetCommand(dotnetOptions, context);
 }
